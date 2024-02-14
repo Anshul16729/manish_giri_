55e517850ec27f1fcfe2f67efc607a0de50507c5
Anshul Shrivastava <anshul.shrivastava@etelligens.in>
2024-02-14 21:26:41 +0530

    UI Completed

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -16,7 +16,8 @@ const Footer = () => {
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ --> */}
-            Designed by <a href="https://www.linkedin.com/in/%F0%9F%91%A9%E2%80%8D%F0%9F%92%BB-anshul-shrivastava-6b1483152" target='_blank'>Anshul Shrivastava</a>
+            Developer <a href="https://www.linkedin.com/in/%F0%9F%91%A9%E2%80%8D%F0%9F%92%BB-anshul-shrivastava-6b1483152" target='_blank'>Anshul Shrivastava</a><br/>
+            Designer <a href="https://www.linkedin.com/in/manish-giri-4192a2158" target='_blank'>Manish Giri</a>
           </div>
         </div>
       </footer>
